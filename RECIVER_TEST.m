628a7180ede09671d4d3571348d71200b5262b6f
Michalstank <michal2stan@gmail.com>
2025-02-06 09:41:42 +0100

    Followed an Example from DSP book no idea what it does, chap: Timing Synchronization 198

--- RECIVER_TEST.m
+++ RECIVER_TEST.m
@@ -2,7 +2,7 @@
 close all
 clear variables
 
-rx = sdrrx('Pluto');
+rx = sdrrx('Pluto', 'SamplesPerFrame',1e6,'OutputDataType','double');
 
 %Check if its connected
 plutoInfo = findPlutoRadio;
@@ -11,14 +11,42 @@ if (plutoInfo.SerialNum == 0)
 end
 
 %% Reciver Config
-rx.CenterFrequency = 2.415e9;
-rx.BasebandSampleRate = 2e6;
-L =20000;
-rx.SamplesPerFrame = L;
-freq = (-L/2:L/2 -1)*(1/rx.BasebandSampleRate) + rx.CenterFrequency
+%rx.CenterFrequency = 2.415e9;
+%rx.BasebandSampleRate = 2e6;
+%L =20000;
+%rx.SamplesPerFrame = L;
+%freq = (-L/2:L/2 -1)*(1/rx.BasebandSampleRate) + rx.CenterFrequency
 
-figure;
 
-    data = rx()
-    f = fftshift(fft(complex(data)));
-    plot(freq, abs(f)/length(f));
+%% Reciver Code
+
+%figure;
+%data = rx()
+%f = fftshift(fft(complex(data)));
+%plot(freq, abs(f)/length(f));
+
+% New Data based on data transmit
+
+rcrFilter = comm.RaisedCosineReceiveFilter('InputSamplesPerSymbol'  , 12, ...
+                                           'DecimationFactor'       , 4);
+
+data = rcrFilter(rx());
+
+% Black Magic
+
+VFD = dsp.VariableFractionalDelay;
+cd = comm.ConstellationDiagram;
+
+rem = 12/4;
+
+data = data(end-rem*1000+1:end);
+
+for index = 0:300
+    tau_hat = index/50;
+    delayedSig = VFD(data, tau_hat);
+    o = sum(reshape(delayedSig,rem,...
+     length(delayedSig)/rem).',2)./rem;
+    
+    cd(o);
+    pause(0.1);
+end
--- TRANSMITTER_TEST.m
+++ TRANSMITTER_TEST.m
@@ -6,29 +6,52 @@ clear variables
 %            'CenterFrequency',915e6, ...
 %            'BasebandSampleRate',1e6);
 
-tx = sdrtx('Pluto');
+tx = sdrtx('Pluto','Gain',-20);
 
 %Check if its connected
 plutoInfo = findPlutoRadio;
 if (plutoInfo.SerialNum == 0)
     return
 end
+
+%% Transmitter Config
+
+%fs = 2e6;
+%tx.BasebandSampleRate = fs;
+%tx.CenterFrequency = 2.415e9;
 %% Waveform Code
+% Creates a sine wave at 200kHz and transmits it at 2.415GHz
 
-fs = 2e6;
-sw = dsp.SineWave;
-sw.Amplitude = 0.5;
-sw.Frequency = 100e3;
-sw.ComplexOutput = true;
-sw.SampleRate = fs;
-sw.SamplesPerFrame = 5000;
-txWaveform = sw(); 
+%sw = dsp.SineWave;
+%sw.Amplitude = 0.5;
+%sw.Frequency = 100e3;
+%sw.ComplexOutput = true;
+%sw.SampleRate = fs;
+%sw.SamplesPerFrame = 5000;
+%txWaveform = sw(); 
 
-%% Program Code
+%% Add Data to Signal
 
-tx.CenterFrequency = 2.415e9;
-tx.BasebandSampleRate = fs;
-tx.Gain = -5;
-transmitRepeat(tx,txWaveform);
+% Following an example
+% Create Random Data
 
-%% Reciver Experiment Lol
+data = randi([0 1], 2^15, 1);
+
+% Apparently Modulate Data:
+
+modData = comm.QPSKModulator('BitInput',true);
+txData  = modData(data);
+
+% Filters
+% Will need to set the filters on specific codes
+
+rctFilter = comm.RaisedCosineTransmitFilter('OutputSamplesPerSymbol', 12);
+
+%Transmit Signal Over Radio
+transmitRepeat(tx,rctFilter(txData));
+%data = rcrFilter(rx());
+
+%% Transmitter Code
+
+%tx.Gain = -5;
+%transmitRepeat(tx,txWaveform);
