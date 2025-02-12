2716c8acc77d6fe72d266c3cf81b938d37d5a41a
Roberto Scardia <roberto.scardia@gmail.com>
2025-02-12 22:56:47 +0100

    added file for power testing

--- measures/transmission_test.m
+++ measures/transmission_test.m
@@ -2,24 +2,47 @@
 %rx = sdrrx('Pluto', ...
 %            'CenterFrequency',915e6, ...
 %            'BasebandSampleRate',1e6);
+clearvars -except Param
+close all
+
 tx = sdrtx('Pluto');
 %Check if its connected
 plutoInfo = findPlutoRadio;
-if (plutoInfo.SerialNum == 0)
-    return
-end
-%% Waveform Code
-fs = 2e6;
-sw = dsp.SineWave;
-sw.Amplitude = 0.5;
-sw.Frequency = 100e3;
-sw.ComplexOutput = true;
-sw.SampleRate = fs;
-sw.SamplesPerFrame = 5000;
-txWaveform = sw(); 
+%if (plutoInfo.SerialNum == 0)
+    %return
+%end
+Fs = 60e6; 								 % Specify the sample rate of the waveform in Hz
+
 %% Program Code
 tx.CenterFrequency = 2.400e9;
-tx.BasebandSampleRate = fs;
-tx.Gain = 0;
-transmitRepeat(tx,txWaveform);
-%% Reciver Experiment Lol
+tx.BasebandSampleRate = 19e6;
+tx.Gain = -10;
+tx.BasebandSampleRate = Fs
+%%
+% QAM configuration
+M = 16; 	 % Modulation order
+% input bit source:
+in = randi([0, 1], 600000, 1);
+
+% Generation
+waveform = qammod(in, M, 'bin', 'InputType', 'bit', 'UnitAveragePower', true);
+
+% Filtering:
+rcFilter = comm.RaisedCosineTransmitFilter('Shape', 'Normal', ...
+    'RolloffFactor', 0.1, ...
+    'OutputSamplesPerSymbol', 30, ...
+    'FilterSpanInSymbols', 32);
+waveform = rcFilter(waveform);
+
+start_freq = 2.4e9;
+stop_freq = 2.8e9;
+delta_freq = stop_freq-start_freq;
+k = ceil(delta_freq/Fs);
+T = timer('TimerFcn',@(~,~)disp(tx.CenterFrequency),'StartDelay',1e-3);
+
+for i = 0:k
+    tx.CenterFrequency = start_freq+(Fs*i);
+    transmitRepeat(tx, waveform);
+    start(T)
+    wait(T)
+end
