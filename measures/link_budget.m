71e28462155e6da63e7d87224ccb71dcb041f707
fredrki <91655281+fredrki@users.noreply.github.com>
2025-04-24 16:05:26 +0200

    Coding gain calculations added

--- measures/link_budget.m
+++ measures/link_budget.m
@@ -16,6 +16,36 @@ target_ber = 1e-6;
 EbNo_vec = 0:0.01:20;
 BER_awgn_qpsk= berawgn(EbNo_vec,'psk',M, 'nondiff');
 
+% BER curve for convolutionally coded QPSK
+trellis = poly2trellis(5, [23 35]); % Standard rate-1/2 convolutional code
+tbl = 20;  % Traceback depth
+rate = 1/2; % Code rate
+Puncture = [1;1;1;1;1;1;1;0];
+
+BER_awgn_qpsk_coded = zeros(size(EbNo_vec));
+num_bits = 1e5; % Number of bits for simulation
+
+for idx = 1:length(EbNo_vec)
+    EbNo = EbNo_vec(idx);
+    snr = EbNo + 10*log10(log2(M)) - 10*log10(rate); % Adjusted for coding rate
+
+    tx_bits = randi([0 1], 1, num_bits);
+
+    encoded_bits = convenc(tx_bits, trellis,Puncture);
+
+    modulated_symbols = pskmod(encoded_bits.', M, pi/M, 'gray',InputType='bit');
+
+    rx_symbols = awgn(modulated_symbols, snr, 'measured');
+
+    demod_bits = pskdemod(rx_symbols, M, pi/M, 'gray',OutputType='bit');
+
+    decoded_bits = vitdec(demod_bits.', trellis, tbl, 'trunc', 'hard',Puncture);
+
+    [~, ber] = biterr(tx_bits, decoded_bits);
+    BER_awgn_qpsk_coded(idx) = ber;
+end
+
+
 
 y = 1;
 
@@ -26,6 +56,16 @@ for i=(1:length(BER_awgn_qpsk))
 end 
 fit = polyfit(EbNo_vec(y-1:y), BER_awgn_qpsk(y-1:y), 1);
 target_EbNo = (target_ber-fit(2))/fit(1);
+
+y_coded = 1;
+for i=(1:length(BER_awgn_qpsk_coded))
+    if(BER_awgn_qpsk_coded(i)> target_ber)
+        y_coded = i;
+    end
+end 
+fit_coded = polyfit(EbNo_vec(y_coded-1:y_coded), BER_awgn_qpsk_coded(y_coded-1:y_coded), 1);
+target_EbNo_coded = (target_ber-fit_coded(2))/fit_coded(1);
+
 
 figure;
 plot(EbNo_vec, log10(BER_awgn_qpsk));
@@ -68,7 +108,7 @@ N0_dB = 10*log10(N0_lin);
 %Bandwidth = 20*MHz;
 
 %NF_transm = Noise_floor_transm -174 + 10*log10(Bandwidth);
-G_ant_dB = 2.1;
+G_ant_dB = 2.0;
 G_ant_lin = 10^(G_ant_dB/20);
 N=30;
 d_0=1;
@@ -106,3 +146,6 @@ EbN0_margin = LB_EbN0 -target_EbNo
 EbN0_margin_24 = LB_EbN0_24 -target_EbNo
 EbN0_margin_900 = LB_EbN0_900 -target_EbNo
 
+Coding_gain = target_EbNo - target_EbNo_coded
+EbN0_margin_24_coded = LB_EbN0_24 - target_EbNo_coded
+EbN0_margin_900_coded = LB_EbN0_900 -target_EbNo_coded
